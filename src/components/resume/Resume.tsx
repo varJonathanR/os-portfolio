8bd0c957be08453a3f2f847fe2be4366dd41bf8b
Jonathan Rodriguez <jonathan20.rodriguez04@gmail.com>
2024-02-07 17:22:59 -0600

    Open on new windows added on Resume

--- src/components/resume/Resume.tsx
+++ src/components/resume/Resume.tsx
@@ -1,6 +1,7 @@
 export default function Resume() {
     return (
-        <section className="w-full h-full">
+        <section className="w-full h-full flex flex-col justify-center items-center">
+            <a href="https://cv-portfolio-varjonathanr.netlify.app/" className="py-2 px-4 rounded-sm mb-2 bg-neutral-800"  target="_blank" rel="noopener noreferrer">Open on a new window</a>
             <iframe src="https://cv-portfolio-varjonathanr.netlify.app/" className="w-full h-full"></iframe>
         </section>
     )
